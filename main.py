30a5a917477fd56c6d32caaf66d160704bf800a8
Nicolas Charrondiere <nicolas.charrondiere@student-cs.fr>
2026-03-18 15:47:43 +0100

    improved main

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ from tools import DataProcessor, ForecastModel
 def main(evaluation_point : int = 30, model_type: str = "random_forest"):
     path_folder = "data/"
     processor = DataProcessor(path_folder)
-    print(processor.df.head())
+    # print(processor.df.head())
     df = processor.run()
 
     forecastModel = ForecastModel(model_type=model_type)
     df_train = df.iloc[:-evaluation_point]
     df_eval = df.iloc[-evaluation_point:]
+    print("Starting to train")
     forecastModel.train(df_train)
+    print("Starting to evaluate")
     forecastModel.evaluate(df_eval)
 
 if __name__ == "__main__":
